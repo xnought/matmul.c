204788ff3aaef6b27df147a4dc754912d795d4f6
xnought <bertuccd@oregonstate.edu>
2023-07-28 17:55:05 -0400

    fix: updates strides when reshaped

--- ops.c
+++ ops.c
@@ -6,18 +6,22 @@ typedef struct {
   int strides[2];
 } Matrix;
 
+void updateStrides(Matrix *m) {
+  if (m->shape[0] < m->shape[1]) {
+    m->strides[0] = m->shape[1];
+    m->strides[1] = 1;
+  } else {
+    m->strides[0] = m->shape[1];
+    m->strides[1] = 1;
+  }
+}
+
 Matrix matrix(double *data, int shape[2]) {
   Matrix m;
   m.data = data;
   m.shape[0] = shape[0];
   m.shape[1] = shape[1];
-  if (m.shape[0] < m.shape[1]) {
-    m.strides[0] = m.shape[1];
-    m.strides[1] = 1;
-  } else {
-    m.strides[0] = m.shape[1];
-    m.strides[1] = 1;
-  }
+  updateStrides(&m);
   return m;
 }
 
@@ -35,6 +39,13 @@ double dot(Matrix a, Matrix b, int length) {
   return summed;
 }
 
+Matrix reshape(Matrix m, int shape[2]) {
+  m.shape[0] = shape[0];
+  m.shape[1] = shape[1];
+  updateStrides(&m);
+  return m;
+}
+
 void printMatrix(Matrix a) {
   for (int i = 0; i < a.shape[0]; i++) {
     for (int j = 0; j < a.shape[1]; j++) {
@@ -48,6 +59,13 @@ int main() {
   double data[6] = {0., 1., 2., 3., 4., 5.};
 
   Matrix a = matrix(data, (int[]){2, 3});
+  printShape(a);
+  printf("\n");
+  printMatrix(a);
+
+  a = reshape(a, (int[]){6, 1});
+  printShape(a);
+  printf("\n");
   printMatrix(a);
 
   return 0;
