9c496ef32607d149083d668e793f5cf0a4cbe1b0
xnought <bertuccd@oregonstate.edu>
2023-07-30 16:13:55 -0400

    switch to 32 bit floats

--- ops.c
+++ ops.c
@@ -3,8 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define float_t float
+
 typedef struct {
-  double *data;
+  float_t *data;
   int shape[2];
   int strides[2];
 } Matrix;
@@ -19,7 +21,7 @@ void updateStrides(Matrix *m) {
   }
 }
 
-Matrix matrix(double *data, int shape[2]) {
+Matrix matrix(float_t *data, int shape[2]) {
   Matrix m;
   m.data = data;
   m.shape[0] = shape[0];
@@ -31,13 +33,13 @@ Matrix matrix(double *data, int shape[2]) {
 int getDataIndex(int stridesI, int stridesJ, int i, int j) {
   return i * stridesI + j * stridesJ;
 }
-double getMatrix(Matrix m, int i, int j) {
+float_t getMatrix(Matrix m, int i, int j) {
   return m.data[i * m.strides[0] + j * m.strides[1]];
 }
-void setMatrix(Matrix *m, int i, int j, double value) {
+void setMatrix(Matrix *m, int i, int j, float_t value) {
   m->data[i * m->strides[0] + j * m->strides[1]] = value;
 }
-void addEqualMatrix(Matrix *m, int i, int j, double value) {
+void addEqualMatrix(Matrix *m, int i, int j, float_t value) {
   m->data[i * m->strides[0] + j * m->strides[1]] += value;
 }
 
@@ -67,7 +69,7 @@ void matmul(Matrix a, Matrix b, Matrix out) {
 #pragma omp parallel for
   for (int k = 0; k < m; k++) {
     for (int j = 0; j < n; j++) {
-      double sum = 0.0;
+      float_t sum = 0.0;
 #pragma omp simd reduction(+ : sum)
       for (int i = 0; i < inner; i++) {
         sum += b.data[b.strides[0] * i + b.strides[1] * j] *
@@ -79,10 +81,10 @@ void matmul(Matrix a, Matrix b, Matrix out) {
   }
 }
 
-double dot(Matrix a, Matrix b) {
+float_t dot(Matrix a, Matrix b) {
   assert(a.shape[1] == b.shape[0]);
   assert(a.shape[0] == 1 && b.shape[1] == 1);
-  double output[1] = {0.0};
+  float_t output[1] = {0.0};
   Matrix out = matrix(output, (int[]){1, 1});
   matmul(a, b, out);
   return out.data[0];
@@ -114,22 +116,23 @@ int main() {
 
   // matrix matrix multiply
   // there are m^3 operations for an mxm matrix multiple with mxm other matrix.
-  double times = 0.0;
-  for (int i = 0; i < 10; i++) {
-    double data[1000000] = {1.0};
-    double output[100000] = {0.0};
+  float_t times = 0.0;
+  int runs = 1;
+  for (int i = 0; i < runs; i++) {
+    float_t data[1000000] = {1.0};
+    float_t output[100000] = {0.0};
     Matrix a = matrix(data, (int[]){1000, 1000});
     Matrix b = matrix(data, (int[]){1000, 1000});
     Matrix out = matrix(output, (int[]){1000, 1000});
 
-    double start = omp_get_wtime();
+    float_t start = omp_get_wtime();
     matmul(a, b, out);
-    double end = omp_get_wtime();
+    float_t end = omp_get_wtime();
     times += (end - start);
   }
-  printf("time %f\n", times / 10);
-  printf("megaflops %f\n", ((1000 * 1000 * 1000) / (times / 10)) / 1e6);
-  printf("gigaflops %f", ((1000 * 1000 * 1000) / (times / 10)) / 1e9);
+  printf("time %f\n", times / runs);
+  printf("megaflops %f\n", ((1000 * 1000 * 1000) / (times / runs)) / 1e6);
+  printf("gigaflops %f", ((1000 * 1000 * 1000) / (times / runs)) / 1e9);
 
   return 0;
 }
